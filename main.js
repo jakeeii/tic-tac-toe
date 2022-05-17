e29f65e9db8910f27ccd5903c2ea6452bbea505a
Jake <jakevisc@gmail.com>
2022-05-17 16:59:29 -0230

    Create check winner/update announcement functions
    I created the functions checkOWinner and checkXWinner to go through
    all possible variations of winning and checking to see if any are
    fulfilled, otherwise the function returns false and the gameOver
    variable stays false to allow the next player to continue. If one of
    these functions returns true, then the winner is displayed and the
    gameOver variable becomes true preventing a next move from being made
    until the board is reset. I also created an updateAnnouncement function
    which updates the announcement near the top of the screen after every
    move to display whose turn it is. This function will also update to show
    the winner if there is a winner. The logic for a draw is not yet
    completed.

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ const tile7 = document.querySelector('.tile7')
 const tile8 = document.querySelector('.tile8')
 const tile9 = document.querySelector('.tile9')
 const restart = document.querySelector('.restart')
+const announcement = document.querySelector('.announcement')
+let welcomeMessage = 'Select a tile to begin. X starts.'
+
 
 const gameboard = (function() {
 
@@ -28,21 +31,29 @@ const gameboard = (function() {
   const playerX = new Player('Player 1', 'X')
   const playerO = new Player('Player 2', 'O')
   let currentPlayer = playerX
+  let gameOver = false
+  let currentPlayerMessage = ''
 
   const placeMark = function(tile, mark) {
-    if (tile.textContent !== "") {
+    if (tile.textContent !== "" || gameOver == true) {
       return
     } else {
       tile.textContent = mark
     }
-    switchPlayers()
+    isGameOver()
+    if (gameOver == false) {
+      switchPlayers()
+      updateAnnouncement(currentPlayerMessage)
+    }
   }
 
   const switchPlayers = function() {
     if (currentPlayer == playerX) {
       currentPlayer = playerO
+      currentPlayerMessage = `It is ${playerO.mark}'s turn.`
     } else {
       currentPlayer = playerX
+      currentPlayerMessage = `It is ${playerX.mark}'s turn`
     }
   }
 
@@ -56,7 +67,73 @@ const gameboard = (function() {
     })
   }
 
-  return { placeMark, clearBoard, getCurrentMark };
+  const restartGame = function() {
+    clearBoard()
+    updateAnnouncement(welcomeMessage)
+    gameOver = false
+  }
+
+  const updateAnnouncement = function(message) {
+    announcement.textContent = message
+  }
+
+  const checkXWinner = function() {
+    if (tile1.textContent == 'X' && tile2.textContent == 'X' && tile3.textContent == 'X') {
+      return true
+    } else if (tile4.textContent == 'X' && tile5.textContent == 'X' && tile6.textContent == 'X') {
+      return true
+    } else if (tile7.textContent == 'X' && tile8.textContent == 'X' && tile9.textContent == 'X') {
+      return true
+    } else if (tile1.textContent == 'X' && tile4.textContent == 'X' && tile7.textContent == 'X') {
+      return true
+    } else if (tile2.textContent == 'X' && tile5.textContent == 'X' && tile8.textContent == 'X') {
+      return true
+    } else if (tile3.textContent == 'X' && tile6.textContent == 'X' && tile9.textContent == 'X') {
+      return true
+    } else if (tile1.textContent == 'X' && tile5.textContent == 'X' && tile9.textContent == 'X') {
+      return true
+    } else if (tile3.textContent == 'X' && tile5.textContent == 'X' && tile7.textContent == 'X') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const checkOWinner = function() {
+    if (tile1.textContent == 'O' && tile2.textContent == 'O' && tile3.textContent == 'O') {
+      return true
+    } else if (tile4.textContent == 'O' && tile5.textContent == 'O' && tile6.textContent == 'O') {
+      return true
+    } else if (tile7.textContent == 'O' && tile8.textContent == 'O' && tile9.textContent == 'O') {
+      return true
+    } else if (tile1.textContent == 'O' && tile4.textContent == 'O' && tile7.textContent == 'O') {
+      return true
+    } else if (tile2.textContent == 'O' && tile5.textContent == 'O' && tile8.textContent == 'O') {
+      return true
+    } else if (tile3.textContent == 'O' && tile6.textContent == 'O' && tile9.textContent == 'O') {
+      return true
+    } else if (tile1.textContent == 'O' && tile5.textContent == 'O' && tile9.textContent == 'O') {
+      return true
+    } else if (tile3.textContent == 'O' && tile5.textContent == 'O' && tile7.textContent == 'O') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const isGameOver = function() {
+    if (checkXWinner() == true) {
+      gameOver = true
+      updateAnnouncement(`${playerX.name} is the winner!`)
+    } else if (checkOWinner() == true) {
+      gameOver = true
+      updateAnnouncement(`${playerO.name} is the winner!`)
+    } else {
+      return
+    }
+  }
+
+  return { placeMark, restartGame, getCurrentMark };
 
 }())
 
@@ -97,5 +174,5 @@ tile9.addEventListener('click', (ev) => {
 })
 
 restart.addEventListener('click', () => {
-  gameboard.clearBoard()
+  gameboard.restartGame()
 })
