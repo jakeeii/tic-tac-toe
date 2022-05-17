2ed946ad845f53768798cded6acab45c35ecd3b4
Jake <jakevisc@gmail.com>
2022-05-17 11:07:16 -0230

    Create factory function and gameboard functions
    I created a factory function to make the players and set their marks to
    either 'O' or 'X'. I also added some functions to the gameboard module
    such as PlaceX and PlaceO which will place the corresponding mark on an
    empty gameboard tile. I also added a restart button which clears the
    gameboard and a startNewGame() function. Will definitely need to refactor
    this code at some point but just working on getting the functionality
    down at the moment.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Make a module with a gameboard object  
 // Store the gameboard as an array
 // Make player objects (factory function or constructor)
+
+// Make two different functions to place a mark on the board
+// Make a function to switch between players
+// Make a function to check if there is a winner after each move on all directions
+
 const tile1 = document.querySelector('.tile1')
 const tile2 = document.querySelector('.tile2')
 const tile3 = document.querySelector('.tile3')
@@ -10,7 +15,62 @@ const tile6 = document.querySelector('.tile6')
 const tile7 = document.querySelector('.tile7')
 const tile8 = document.querySelector('.tile8')
 const tile9 = document.querySelector('.tile9')
+const restart = document.querySelector('.restart')
 
 const gameboard = (function() {
-  let board = [tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9]
+
+  const board = [tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9]
+
+  const placeX = function(tile) {
+    if (tile.textContent !== "") {
+      return
+    } else {
+      tile.textContent = "X"
+    }
+  }
+
+  const placeO = function(tile) {
+    if (tile.textContent !== "") {
+      return
+    } else {
+      tile.textContent = "O"
+    }
+  }
+
+  const getCurrentPlayer = function() {
+
+  } 
+
+  const startNewGame = function() {
+    const playerX = Player('Player 1', 'X')
+    const playerO = Player('Player 2', 'O')
+  }
+
+  const clearBoard = function() {
+    board.forEach((tile) => {
+      tile.textContent = ""
+    })
+  }
+
+  return { placeO, placeX, startNewGame, clearBoard };
+
 }())
+
+const Player = (name, mark) => {
+  const getName = () => name
+  const getMark = () => mark
+
+  return { getName, getMark }
+}
+  
+tile5.addEventListener('click', (ev) => {
+  gameboard.placeO(ev.target)
+})
+
+tile8.addEventListener('click', (ev) => {
+  gameboard.placeO(ev.target)
+})
+
+restart.addEventListener('click', () => {
+  gameboard.clearBoard()
+})
